ecde8a20a917dc87a6a9ab56b70d709ba8199052
Pedro Gama <pedro.gama@ufms.br>
2025-09-22 02:17:41 -0400

    Adicionado seção “Sobre Mim” arrastável com interface de usuário estilizada
    Introduzido uma nova seção “Sobre Mim” com caixas de foto e texto arrastáveis com botões SVG personalizados e estilo. Adicionado os recursos necessários, atualizado as importações Angular para a funcionalidade de arrastar e soltar e aprimorado o layout e a aparência para melhorar a interatividade.

--- src/app/sobre-mim/sobre-mim.ts
+++ src/app/sobre-mim/sobre-mim.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CdkDrag, CdkDropList  } from '@angular/cdk/drag-drop';
+
+/** * @title Basic Drag&Drop */
 
 @Component({
   selector: 'app-sobre-mim',
-  imports: [],
+  imports: [CdkDrag, CommonModule],
   templateUrl: './sobre-mim.html',
   styleUrl: './sobre-mim.scss',
+  standalone: true,
   exportAs: 'sobreMim'
 })
 export class SobreMim {
-
 }
