6afd8b0dcb79297f6dac2f2eeab6d2a303f9722d
Pedro Gama <pedro.gama@ufms.br>
2025-09-22 00:18:21 -0400

    Adicionar componentes de seção e navegação dinâmica
    Criado componentes 'sobre-mim', 'projetos', e 'experiencia'. Refatorando o aplicativo principal para usar um array dinâmico de botões para navegação entre seções. Atualizado o estilo dos botões ativos/inativos e migração para [ngClass] do Angular para vinculação de classes. Também adiciona @angular/cdk como dependência.

---
+++ src/app/experiencia/experiencia.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiencia',
+  imports: [],
+  templateUrl: './experiencia.html',
+  styleUrl: './experiencia.scss',
+  exportAs: 'experiencia'
+})
+export class Experiencia {
+
+}
